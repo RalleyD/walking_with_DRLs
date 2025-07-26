288ffc3fe56832c9124a9fbe8573008af89aca34
RalleyD <0ralld67@solent.ac.uk>
2025-07-26 15:43:07 +0100

    wip - performance metrics

--- src/evaluate/performance_metrics.py
+++ src/evaluate/performance_metrics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from custom_logger import CustomLogger
 
 
@@ -38,7 +40,7 @@ class PerformanceMetrics:
         Average return in the 90 % stable target.
     """
 
-    def __init__(self, ema_alpha=0.5, rolling_window_size=100, convergence_thresh=0.9):
+    def __init__(self, ema_alpha=0.1, rolling_window_size=100, convergence_thresh=0.9):
         self._ema_alpha = ema_alpha
         self._rolling_window_size = rolling_window_size
         self._convergence_threshold = convergence_thresh
@@ -49,33 +51,46 @@ class PerformanceMetrics:
 
         # computed metrics
         self._ema_returns = []
+        self._rolling_var = []
+        self._rolling_var_coef = []
         self._best_performance = None
         self._convergence_episode = None
 
-    def update(self, episode_return: float):
+    def update(self, episode_return: float, policy_entropy: float = None):
         """
         Called after each training episode/epoch.
         Update raw data.
         Compute exponential moving average.
         """
         self._episode_returns.append(episode_return)
+
+        if policy_entropy:
+            self._policy_entropies.append(policy_entropy)
+
         # TODO refactor
-        if len(self._ema_returns) == 0:
+        if not self._ema_returns:
             self._ema_returns.append(self._episode_returns[-1])
             return
 
-        self._ema_returns.append(
-            self._episode_returns[-1] *
-            self._ema_alpha + (1 - self._ema_alpha) *
+        ema = self._episode_returns[-1] * \
+            self._ema_alpha + (1 - self._ema_alpha) * \
             self._ema_returns[-1]
+
+        self._ema_returns.append(
+            ema
         )
+
+        self._best_performance = max(self._ema_returns)
+
+        if len(self._episode_returns)-1 % self._rolling_window_size == 0:
+            # update rolling variance
+            self._stability_metrics()
 
     def stable_convergence_time(self):
         """
         Find when EMA first reaches and stays above target.
         This two-pointer algorithm determines the longest
         sustained period (n episodes) in the EMA returns list.
-        where the EMA stayed above the target threshold.
         The method self-determines the stable 'sustained' duration.
         This adds fairness to an agent with high training variance,
         and where different models may reach convergence for different
@@ -110,16 +125,56 @@ class PerformanceMetrics:
         logger.info(f"\nLongest n_episodes above {target=}: {count_a}")
         logger.info("    Convergence after %d episodes." % convergence_point)
 
-        return convergence_point
+        self._convergence_episode = convergence_point
 
-    def stability_metrics(sefl):
+        return self._convergence_episode
+
+    def _stability_metrics(self):
         """
-        Rolling variance and coefficient of variance.
+        Rolling returns variance and coefficient of variance.
         """
-        pass
+        # option 1, perform incrementally from a call to update
+        returns_window_np = np.array(
+            self._episode_returns[-self._rolling_window_size:])
+
+        var = returns_window_np.var()
+        self._rolling_var.append(var)
+
+        var_coeff = np.sqrt(var) / returns_window_np.mean()
+        self._rolling_var_coef.append(var_coeff)
+
+        # option 2, perform sliding windows on the final episode returns array - slow
+
+    def exploration_trend(self):
+        """
+        Track how agent exploration changes over time
+        """
+        # calculate moving average of entropy
+        entropy_ma = 0
+
+        if self._policy_entropies:
+            weights = np.ones_like(self._rolling_window_size) / \
+                self._rolling_window_size
+            entropy_ma = np.convolve(
+                self._policy_entropies, weights, mode='valid')
+
+        return entropy_ma
 
     def sample_efficiency(self):
         """
         Episodes taken to reach convergance target.
         """
-        pass
+        # TODO refactor duplication
+        target = self._best_performance * self._convergence_threshold
+
+        ema_above_target = np.where(self._ema_returns >= target)
+
+        if len(ema_above_target):
+            sample_eff = ema_above_target[0]
+        else:
+            sample_eff = 0
+
+        return sample_eff
+
+    def get_stability_metrics(self) -> tuple:
+        return self._rolling_var, self._rolling_var_coef
