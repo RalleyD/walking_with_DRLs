62767f8cf0d84f624c8fb178a432a364c404f783
RalleyD <0ralld67@solent.ac.uk>
2025-07-27 22:31:23 +0100

    plotter - create summary training figure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 from src.reinforce.reinforce_agent import ReinforceAgent
 from src.reinforce.reinforce_trainer import ReinforceTrainer
-from src.util.plotter import learning_rate_ma
+from src.util.plotter import learning_rate_ma, evaluation_figure_a_b
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -18,7 +18,7 @@ GAMMA = 0.99    # discount factor on future steps
 ################################
 # Hyperparameters - walker2D
 ################################
-EPOCHS_WALKER = 25000   # episodes
+EPOCHS_WALKER = 200   # episodes
 HIDDEN_LYR_1_WALKER = 128
 HIDDEN_LYR_2_WALKER = 128
 # use a low learning rate because the high variance will cause large gradient updates.
@@ -45,7 +45,7 @@ def train_reinforce(epochs: int,
 
     trainer = ReinforceTrainer(sim_env, reinforce_agent, epochs)
 
-    return trainer.train()
+    return trainer.train(), trainer
 
 ################################################################
 
@@ -55,19 +55,33 @@ def train_reinforce(epochs: int,
 #                 'InvertedPendulum-v4')
 
 
-walker_returns = train_reinforce(EPOCHS_WALKER,
-                                 HIDDEN_LYR_1_WALKER,
-                                 HIDDEN_LYR_2_WALKER,
-                                 LR_WALKER, GAMMA_WALKER,
-                                 "Walker2d-v4",
-                                 MAX_GRADIENT_NORM)
+walker_returns, reinforce_trainer = train_reinforce(EPOCHS_WALKER,
+                                                    HIDDEN_LYR_1_WALKER,
+                                                    HIDDEN_LYR_2_WALKER,
+                                                    LR_WALKER, GAMMA_WALKER,
+                                                    "Walker2d-v4",
+                                                    MAX_GRADIENT_NORM)
 
 ################################################################
 
 x = np.arange(0, len(walker_returns), 1)
 y = walker_returns
 
-learning_rate_ma(x, np.array(
-    y), title=f"Reinforce Learning Curve, layers: {HIDDEN_LYR_1_WALKER}, {HIDDEN_LYR_2_WALKER}")
+# rolling_var, var_coeff = reinforce_trainer.metrics.get_stability_metrics()
+
+# h line
+# target_reached = reinforce_trainer.metrics.sample_efficiency()
+
+# h line
+# stable_convergence = reinforce_trainer.metrics.stable_convergence_time()
+
+
+# learning_rate_ma(x, np.array(y),
+#                  #  target_ep=target_reached,
+#                  convergence_ep=stable_convergence,
+#                  title=f"Reinforce Learning Curve, layers: {HIDDEN_LYR_1_WALKER}, {HIDDEN_LYR_2_WALKER}"
+#                  )
+
+evaluation_figure_a_b(metrics_a=reinforce_trainer.metrics)
 
 #################################################################
--- src/util/plotter.py
+++ src/util/plotter.py
@@ -4,12 +4,87 @@ from typing import Optional
 from pathlib import Path
 from moviepy import ImageSequenceClip
 from datetime import datetime
+from src.evaluate.performance_metrics import PerformanceMetrics
 
 FILE_DIR = Path(__file__).parent
 PRJ_ROOT = Path(FILE_DIR).parent.parent
 
 
-def learning_rate_ma(x: Optional[np.array], y: np.array, title: Optional[str] = "Reinforce learning curve"):
+"""create wrapper function that will:
+    take a dict of model: metrics
+    create a subplot column for each model
+    if each model has variance coeffecient data, create a third column
+    rows corresponding to the number of metrics in each model key
+    plot learning rate with moving average and convergence vlines
+    plot exploration rate below each model's learning curve graph
+    plot the rolling variance below
+    in the middle column, top row, in between both models
+    plot both model's variance coefficient"""
+
+
+def evaluation_figure_a_b(metrics_a: PerformanceMetrics,
+                          model_a_name: str = "REINFORCE",
+                          metrics_b: PerformanceMetrics = None,
+                          model_b_name: str = "TD3"):
+    rows = 3
+    cols = 2
+
+    # combine the right-most subplots into one
+
+    fig, axs = plt.subplots(rows, cols, figsize=(14, 9))
+    gs = axs[2, 0].get_gridspec()
+    # a/b multi-plot row
+    for ax in axs[-1, 0:]:
+        ax.remove()
+    ax_ab = fig.add_subplot(gs[-1, 0:])
+    ax_ab.set_title(f"{name}: A/B Training stability - Normalised variance")
+    ax_ab.grid(visible=True)
+    ax_ab.legend()
+
+    for col, (model_metrics, name) in enumerate([(metrics_a, model_a_name),
+                                                (metrics_b, model_b_name)]):
+
+        # TODO refactor as inner loop
+        if model_metrics is None:
+            continue
+        # plot learning curves
+        convergence = model_metrics.stable_convergence_time()
+        y = model_metrics._episode_returns
+        _lc_axis(x=None, y=y, target_ep=None, convergence_ep=convergence,
+                 title=f"{name}: Learning Curve",
+                 ax=axs[0, col])
+
+    # TODO refactor as inner loop
+    for col, (model_metrics, name) in enumerate([(metrics_a, model_a_name),
+                                                (metrics_b, model_b_name)]):
+        if model_metrics is None:
+            continue
+        # plot exploration rates
+        exploration_sma = model_metrics.exploration_trend()
+        x = np.arange(model_metrics._rolling_window_size,
+                      model_metrics._rolling_window_size + len(exploration_sma))
+        axs[1, col].plot(
+            x, exploration_sma, label=f"rolling av window: {model_metrics._rolling_window_size}")
+        axs[1, col].set_title(f"{name}: Exploration Rate")
+        axs[1, col].legend()
+
+    # TODO refactor as inner loop
+    for (model_metrics, name) in [(metrics_a, model_a_name),
+                                  (metrics_b, model_b_name)]:
+
+        if model_metrics is None:
+            continue
+        # plot coefficient of variance
+        _, var_coeff = model_metrics.get_stability_metrics()
+        print(f"{var_coeff=}")
+        x = np.arange(model_metrics._rolling_window_size,
+                      len(var_coeff) * model_metrics._rolling_window_size + 1)
+        print(f"{x=}")
+        ax_ab.plot(
+            x, var_coeff, 'bo')
+        ax_ab.legend()
+
+    plt.show()
 
 
 def learning_rate_ma(x: Optional[np.array], y: np.array,
@@ -17,10 +92,24 @@ def learning_rate_ma(x: Optional[np.array], y: np.array,
                      convergence_ep: int = None,
                      title: Optional[str] = "Reinforce learning curve"):
 
+    fig, ax = plt.subplots(1, 1)
+
+    _lc_axis(x, y, target_ep, convergence_ep, title, ax)
+
+    output_dir = PRJ_ROOT / "plots"
+    output_dir.mkdir(exist_ok=True)
+    out_file = title.replace(" ", "-").replace(",",
+                                               "_").replace(":", "") + datetime.now().strftime("%Y-%m-%d_%H_%M_%S") + ".png"
+
+    fig.savefig(output_dir / out_file, dpi=600)
+
+    plt.show()
+
+
+def _lc_axis(x, y, target_ep, convergence_ep, title, ax):
     if x is None:
         x = np.arange(0, len(y), 1)
 
-    fig, ax = plt.subplots(1, 1)
     ax.plot(x, y, label="raw returns")
 
     # target first reached
@@ -39,21 +128,11 @@ def learning_rate_ma(x: Optional[np.array], y: np.array,
 
     ax.plot(sma_x, sma, label=f"moving average, {window=}")
 
-
     ax.grid(visible=True)
     ax.legend()
     ax.set_title(title)
     ax.set_xlabel("episode (n)")
     ax.set_ylabel("returns (n)")
-
-    output_dir = PRJ_ROOT / "plots"
-    output_dir.mkdir(exist_ok=True)
-    out_file = title.replace(" ", "-").replace(",",
-                                               "_").replace(":", "") + datetime.now().strftime("%Y-%m-%d_%H_%M_%S") + ".png"
-
-    fig.savefig(output_dir / out_file, dpi=600)
-
-    plt.show()
 
 
 def record_gif(gif_frames: list, fps=200, filename: str = "reinforce", epochs: int = None) -> None:
@@ -66,3 +145,9 @@ def record_gif(gif_frames: list, fps=200, filename: str = "reinforce", epochs: i
 
     clip = ImageSequenceClip(gif_frames, fps=fps)
     clip.write_gif(out_dir / file, fps=fps)
+
+
+if __name__ == "__main__":
+    model_a = PerformanceMetrics()
+    model_b = PerformanceMetrics()
+    evaluation_figure_a_b(model_a, metrics_b=model_b)
